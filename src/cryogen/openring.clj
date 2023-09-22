b8f210974e04d11bcfe8de76a7cdc0c29bc11f2a
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-09-22 19:34:16 +0200

    Propagate openring errors

--- src/cryogen/openring.clj
+++ src/cryogen/openring.clj
@@ -2,6 +2,9 @@
   (:require [clojure.java.shell :as sh]))
 
 (defn generate-webring! [{:keys [source-file template-file output-file]}]
-  (->> (sh/sh "go" "run" "-C" "openring" "openring.go" "-S" source-file :in (slurp template-file))
-       :out
-       (spit output-file)))
+  (let [{:keys [exit err out]} (sh/sh "go" "run" "-C" "openring" "openring.go" "-S" source-file :in (slurp template-file))]
+    (if (zero? exit)
+      (spit output-file out)
+      (binding [*out* *err*]
+        (println err)
+        (System/exit exit)))))
