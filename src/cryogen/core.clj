50f886edeb74f6e041a8e6fa5ef5294c39fe6bc0
JohnnyJayJay <johnnyjayjay02@gmail.com>
2024-03-29 12:47:03 +0100

    make now page update optional

--- src/cryogen/core.clj
+++ src/cryogen/core.clj
@@ -9,23 +9,28 @@
 
 (defn compile-site []
   (compile-assets-timed
+   (cond->
     {:update-article-fn
      (fn [article config]
-       ;; Skip articles with `:ignore? true` in metadata
+              ;; Skip articles with `:ignore? true` in metadata
        (when-not (:ignore? article)
-         (hl/highlight-code-in-article (al/autolink-headings article config) config)))
-     :extend-params-fn
-     (fn [{{{:keys [instance user]} :bookwyrm} :now :as params} _site-data]
-       (println "Fetching bookshelf and spotify data")
-       (try
-         (-> params
-           (assoc-in [:now :books] (now/fetch-bookwyrm-books! instance user))
-           (assoc-in [:now :spotify] (now/fetch-spotify-top! (System/getenv "SPOTIFY_ACCESS_TOKEN"))))
-         (catch Exception e
-           (let [{:keys [uri status body]} (ex-data e)]
-             (println "Error preparing now page: status" status "for URI" uri)
-             (pprint body)
-             params))))}))
+         (hl/highlight-code-in-article (al/autolink-headings article config) config)))}
+
+     ;; Fetch now page data if env var is set
+     (System/getenv "UPDATE_NOW_PAGE")
+     (assoc
+      :extend-params-fn
+      (fn [{{{:keys [instance user]} :bookwyrm} :now :as params} _site-data]
+        (println "Fetching bookshelf and spotify data")
+        (try
+          (-> params
+              (assoc-in [:now :books] (now/fetch-bookwyrm-books! instance user))
+              (assoc-in [:now :spotify] (now/fetch-spotify-top! (System/getenv "SPOTIFY_ACCESS_TOKEN"))))
+          (catch Exception e
+            (let [{:keys [uri status body]} (ex-data e)]
+              (println "Error preparing now page: status" status "for URI" uri)
+              (pprint body)
+              params))))))))
 
 (defn -main []
   (load-plugins)
